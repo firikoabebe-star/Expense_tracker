41ec19483cd52d322326bb2c23346ac80f807af1
Firiko <firikoabebe@gmail.com>
2025-09-24 17:28:16 +0300

    database started 2

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,4 +1,4 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 const sql = neon(process.env.DATABASE_URL);
-const db = drizzle({ client: sql });
+const db = drizzle(sql, {schema});
