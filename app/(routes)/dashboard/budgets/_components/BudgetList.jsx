38e25a3d447ac37ee1746a8013365b7f7991b64c
Firiko <firikoabebe@gmail.com>
2025-09-25 17:23:03 +0300

    imoji added

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,9 +1,11 @@
   import React from 'react'
-  
+  import CreateBudget from './CreateBudget.jsx'
   function BudgetList() {
     return (
-      <div>
-        BudgetList
+      <div className='mt-7'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <CreateBudget/>
+            </div>
       </div>
     )
   }
--- app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../../../../components/ui/dialog"
+import EmojiPicker from 'emoji-picker-react'
+import { Button } from '../../../../../components/ui/button';
 
 function CreateBudget() {
+  const [emojiIcon, setEmojiIcon] = useState('😊');
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+
   return (
-    <div>CreateBudget</div>
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className='bg-slate-100 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md'>
+            <h2 className='text-3xl'>+</h2>
+            <h2>Create New Budget</h2>
+          </div>
+        </DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Budget</DialogTitle>
+            {/* Use asChild so DialogDescription doesn't force <p> */}
+            <DialogDescription asChild>
+              <div>
+                <Button
+                  variant="outline"
+                  onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+                >
+                  {emojiIcon}
+                </Button>
+
+                {openEmojiPicker && (
+                  <div className="relative mt-2">
+                    <div className="absolute z-50">
+                      <EmojiPicker
+                        onEmojiClick={(emojiObject) => setEmojiIcon(emojiObject.emoji)}
+                      />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   )
 }
 
